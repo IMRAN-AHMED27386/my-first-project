c72642b1bd620dcc90051a0bfb6338d5e389307e
IMRAN AHAMAD <imran27386@gmail.com>
2025-06-20 05:30:33 +0530

    add dom.html and dom.js files

---
+++ dom.js
@@ -0,0 +1,7 @@
+function changeHeading() {
+  const heading = document.getElementById("main-heading");
+  heading.textContent = "You clicked the button!";
+  
+  const output = document.getElementById("output");
+  output.textContent = "DOM accessed ✅ JavaScript is working!";
+}
